0283c41b5e73a9c82d45bb8e60e41e2a5925e9a3
Stephen Hodgson <hodgson.designs@gmail.com>
2024-07-26 17:23:21 -0400

    fix history pagination

--- Runtime/UI/HistoryPanel.cs
+++ Runtime/UI/HistoryPanel.cs
@@ -28,11 +28,19 @@ namespace BlockadeLabsSDK
         private ScrollRect _scrollRect;
 
         private int _pageSize = 9;
+        private int _itemsProcessed;
         private bool _isFetchingHistory;
         private SkyboxHistory _lastHistoryResult;
         private SkyboxHistoryParameters _lastQueryParams;
 
-        private readonly List<HistoryItemBehaviour> _historyItems = new List<HistoryItemBehaviour>();
+        private readonly Dictionary<int, HistoryItemBehaviour> _historyItems = new Dictionary<int, HistoryItemBehaviour>();
+
+#if !UNITY_2022_1_OR_NEWER
+        private System.Threading.CancellationTokenSource _destroyCancellationTokenSource = new System.Threading.CancellationTokenSource();
+        // ReSharper disable once InconsistentNaming
+        // this is the same name as the unity property introduced in 2022+
+        private System.Threading.CancellationToken destroyCancellationToken => _destroyCancellationTokenSource.Token;
+#endif
 
         private void OnValidate()
         {
@@ -59,6 +67,14 @@ namespace BlockadeLabsSDK
             ClearHistory();
         }
 
+#if !UNITY_2022_1_OR_NEWER
+        private void OnDestroy()
+        {
+            _destroyCancellationTokenSource?.Cancel();
+            _destroyCancellationTokenSource?.Dispose();
+        }
+#endif
+
         private async void OnScrollRectValueChanged(Vector2 scrollPosition)
         {
             if (_lastHistoryResult is { HasMore: true } &&
@@ -66,7 +82,7 @@ namespace BlockadeLabsSDK
                 scrollPosition.y <= 0)
             {
                 _lastQueryParams ??= new SkyboxHistoryParameters();
-                _lastQueryParams.Offset = _historyItems.Count / _pageSize;
+                _lastQueryParams.Offset = _itemsProcessed / _pageSize;
                 await FetchHistoryAsync(_lastQueryParams, clearResults: false);
             }
         }
@@ -99,16 +115,17 @@ namespace BlockadeLabsSDK
 
                 searchParameters ??= _lastQueryParams ??= new SkyboxHistoryParameters();
                 searchParameters.Limit = _pageSize;
-                _lastHistoryResult = await BlockadeLabsSkyboxGenerator.BlockadeLabsClient.SkyboxEndpoint.GetSkyboxHistoryAsync(_lastQueryParams = searchParameters);
+                _lastHistoryResult = await BlockadeLabsSkyboxGenerator.BlockadeLabsClient.SkyboxEndpoint.GetSkyboxHistoryAsync(_lastQueryParams = searchParameters, destroyCancellationToken);
 
                 foreach (var item in _lastHistoryResult.Items)
                 {
-                    if (item.Status != Status.Complete) { continue; }
+                    _itemsProcessed++;
+                    if (_historyItems.ContainsKey(item.Id) || item.Status != Status.Complete) { continue; }
 
                     var historyItemBehaviour = Instantiate(_historyItemPrefab, _historyItemsContainer);
                     historyItemBehaviour.gameObject.SetActive(false);
                     historyItemBehaviour.SetItemData(item, OnHistoryItemClick, OnHistoryItemDelete, OnHistoryItemDownload);
-                    _historyItems.Add(historyItemBehaviour);
+                    _historyItems.Add(item.Id, historyItemBehaviour);
                 }
             }
             catch (Exception e)
@@ -119,8 +136,6 @@ namespace BlockadeLabsSDK
             {
                 _isFetchingHistory = false;
             }
-
-            OnScrollRectValueChanged(_scrollRect.normalizedPosition);
         }
 
         private void ClearHistory()
@@ -129,10 +144,11 @@ namespace BlockadeLabsSDK
 
             foreach (var historyItem in _historyItems)
             {
-                Destroy(historyItem.gameObject);
+                Destroy(historyItem.Value);
             }
 
             _historyItems.Clear();
+            _itemsProcessed = 0;
         }
 
         private void OnHistoryItemClick(SkyboxInfo imagineResult)
@@ -144,11 +160,18 @@ namespace BlockadeLabsSDK
                 "Are you sure you want to remove your skybox? This is not reversible.",
                 onConfirm: async () =>
                 {
-                    var result = await BlockadeLabsSkyboxGenerator.BlockadeLabsClient.SkyboxEndpoint.DeleteSkyboxAsync(skybox);
-
-                    if (result)
+                    try
                     {
-                        await FetchHistoryAsync();
+                        var result = await BlockadeLabsSkyboxGenerator.BlockadeLabsClient.SkyboxEndpoint.DeleteSkyboxAsync(skybox, destroyCancellationToken);
+
+                        if (result)
+                        {
+                            await FetchHistoryAsync();
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.LogException(e);
                     }
                 },
                 onCancel: () =>
@@ -158,11 +181,14 @@ namespace BlockadeLabsSDK
 
         private async void OnHistoryItemDownload(SkyboxInfo skybox)
         {
-#if UNITY_EDITOR
-            await _runtimeGuiManager.Generator.DownloadResultAsync(skybox, false);
-#else
-            await Task.CompletedTask;
-#endif
+            try
+            {
+                await _runtimeGuiManager.Generator.DownloadResultAsync(skybox, false, destroyCancellationToken);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
         }
     }
 }
